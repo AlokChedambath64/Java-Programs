e53c3af63f3306e6c9045ba26b8b1c8c1ba692d2
Alok Chedambath <alokchedambath1@gmail.com>
2023-06-14 02:33:16 +0530

    Create diagonalsum(O(n2)).java

---
+++ diagonalsum(O(n2)).java
@@ -0,0 +1,21 @@
+public class diagonalsum {
+
+    public static int diagonalsum(int matrix[][]){
+        int total = 0;
+        for (int i = 0; i < matrix.length; i++){
+            for (int j = 0; j < matrix[0].length; j++){
+                if (i == j){
+                    total += matrix[i][j];
+                }
+                else if (i + j == matrix.length - 1){
+                    total += matrix[i][j];
+                }
+            }
+        }
+        return total;
+    }
+    public static void main(String args[]){
+        int matrix[][] = {{1, 2, 3, 4,},{5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+        System.out.println(diagonalsum(matrix));
+    }
+}
